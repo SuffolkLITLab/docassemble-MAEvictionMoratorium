50d40dcba7e5aec646e08e9d5d51d398d805df32
System Administrator <qsteenhuis@gmail.com>
2020-06-03 17:38:04 -0400

    Fix problem with showifdef('') and signatures

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MAEvictionMoratorium',
-      version='0.0.4',
+      version='0.0.5',
       description=('A docassemble extension.'),
       long_description='# docassemble.MAEvictionMoratorium\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
