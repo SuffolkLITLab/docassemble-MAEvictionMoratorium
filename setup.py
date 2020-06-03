3c865fdbcef266d04794414f93ab3b3b7439b2aa
System Administrator <qsteenhuis@gmail.com>
2020-06-03 09:50:48 -0400

    Adjusted language about court/download and additional reference to MADE

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MAEvictionMoratorium',
-      version='0.0.1',
+      version='0.0.2',
       description=('A docassemble extension.'),
-      long_description='# docassemble.MAEvictionMoratorium\n\nA docassemble extension.\n\n## Author\n\nSystem Administrator, admin@admin.com\n\n',
+      long_description='# docassemble.MAEvictionMoratorium\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Quinten Steenhuis',
       author_email='qsteenhuis@suffolk.edu',
