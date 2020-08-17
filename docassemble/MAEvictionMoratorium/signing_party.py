24f34725d20934303fe6008ee4e63915f70b7da6
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-08-17 17:18:08 -0400

    Adjust var names (add PDF? Or was that previous?)

--- docassemble/MAEvictionMoratorium/signing_party.py
+++ docassemble/MAEvictionMoratorium/signing_party.py
@@ -2,23 +2,23 @@ from docassemble.base.util import log, DARedis, Individual#, DAObject
 
 redis = DARedis()
   
-def amend_signer( data, key, value ):
-  if 'interview_data_id' in data:
-    party_id = data['party_id']
-    action_data = redis.get_data( data['interview_data_id'] )
+def amend_signer( data_keys, key, value ):
+  if 'signature_data_id' in data_keys:
+    party_id = data_keys['party_id']
+    action_data = redis.get_data( data_keys['signature_data_id'] )
 
     # set party key
     action_data['parties'][ party_id ][ key ] = value
-    redis.set_data( data[ 'interview_data_id' ], action_data )
+    redis.set_data( data_keys[ 'signature_data_id' ], action_data )
     return action_data['parties'][ party_id ]
   else:
     return None
   
-def get_signer( data ):
-  if 'interview_data_id' in data:
-    party_id = data['party_id']
-    if party_id in redis.get_data( data['interview_data_id'] )['parties']:
-      return redis.get_data( data['interview_data_id'] )['parties'][ party_id ]
+def get_signer( data_keys ):
+  if 'signature_data_id' in data_keys:
+    party_id = data_keys['party_id']
+    if party_id in redis.get_data( data_keys['signature_data_id'] )['parties']:
+      return redis.get_data( data_keys['signature_data_id'] )['parties'][ party_id ]
     else:
       return False
   else:
