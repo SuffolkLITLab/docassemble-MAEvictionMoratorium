f351cb2e0ce0c13d58c5903fde80d165a14d522e
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-08-17 21:28:32 -0400

    Refactor maybe. Rename? Repair? Not sure.

--- docassemble/MAEvictionMoratorium/signing_party.py
+++ docassemble/MAEvictionMoratorium/signing_party.py
@@ -2,23 +2,21 @@ from docassemble.base.util import log, DARedis, Individual#, DAObject
 
 redis = DARedis()
   
-def amend_signer( data_keys, key, value ):
-  if 'signature_data_id' in data_keys:
-    party_id = data_keys['party_id']
-    action_data = redis.get_data( data_keys['signature_data_id'] )
+def amend_signer( signature_data_id, party_id, key, value ):
+  if signature_data_id and party_id:
+    signature_data = redis.get_data( signature_data_id )
 
     # set party key
-    action_data['parties'][ party_id ][ key ] = value
-    redis.set_data( data_keys[ 'signature_data_id' ], action_data )
-    return action_data['parties'][ party_id ]
+    signature_data['parties'][ party_id ][ key ] = value
+    redis.set_data( signature_data_id, signature_data )
+    return signature_data['parties'][ party_id ]
   else:
     return None
   
-def get_signer( data_keys ):
-  if 'signature_data_id' in data_keys:
-    party_id = data_keys['party_id']
-    if party_id in redis.get_data( data_keys['signature_data_id'] )['parties']:
-      return redis.get_data( data_keys['signature_data_id'] )['parties'][ party_id ]
+def get_signer( signature_data_id, party_id ):
+  if signature_data_id and party_id:
+    if party_id in redis.get_data( signature_data_id )['parties']:
+      return redis.get_data( signature_data_id )['parties'][ party_id ]
     else:
       return False
   else:
